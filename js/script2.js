10ba236aa2ac98b0bec59474f44c9fedd4978a87
= <=>
2020-06-24 00:52:28 +0200

    modified some lines

--- js/script2.js
+++
@@ -1,7 +0,0 @@
-// adding addEventListener to an html element
-
-document.getElementById("btn").addEventListener("click", displayDate);
-
-function displayDate() {
-    document.getElementsByClassName("demo").innerHTML = Date();
-}
